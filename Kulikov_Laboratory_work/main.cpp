dc310635ff36d456df45abecc59ff6467c383985
Artyom Kulikov <113008949+DarkCre@users.noreply.github.com>
2022-09-26 09:59:48 +0300

    Оптимизирована "защита от дурака", убрана очистка консоли,  место хранения трубы перенесено на более высокий уровень (ближе к main)
    Оптимизирована "защита от дурака" - созданы универсальные функции для 2х типов переменных, убрана,  место хранения трубы перенесено на более высокий уровень (ближе к main) - обоснованно тем, что при хранении ниже необходимая информацию умирает, вместе с окончанием функции, блокируя тем самым возможность редактирования данной информации

--- Kulikov_Laboratory_work/main.cpp
+++ Kulikov_Laboratory_work/main.cpp
@@ -16,6 +16,36 @@ struct Сs //создание структуры компрессорной станции
 	double Effectiveness; //эффективность КС
 };
 
+bool CheckingPositiveDouble(const double& Double) //проверка на правильность ввода положительных переменных типа double
+{
+	if (cin.fail()|| cin.peek() != '\n') //проверка корректности данных
+	{
+		cin.clear();//сбрасываем биты ошибок
+		cin.ignore(1000, '\n');//сбрасываем буфер с некорректными данными
+		cout << "Введено некоректное число, пожалуйста, повторите попытку." << endl;
+		return 0;
+	}
+	else if (Double <= 0.)
+	{
+		cout << "Это значение не может быть отрицательным или равным 0, пожалуйста, повторите попытку." << endl;
+		return 0;
+	}
+	return 1;
+}
+
+
+bool CheckingBool(const bool& Bool) //проверка на правильность ввода переменных типа bool
+{
+	if (cin.fail() || cin.peek() != '\n' || (Bool != 0 && Bool != 1)) //проверка корректности данных
+	{
+		cin.clear();//сбрасываем биты ошибок
+		cin.ignore(1000, '\n');//сбрасываем буфер с некорректными данными
+		cout << "Указано некоректное число, пожалуйста, введите 0 или 1." << endl;
+		return 0;
+	}
+	return 1;
+}
+
 void TextSharedConsole() //текстовая часть консоли
 {
 	cout<< "1. Добавить трубу" << endl
@@ -28,86 +58,77 @@ void TextSharedConsole() //текстовая часть консоли
 		<< "0. Выход" << endl;
 }
 
+
+
 void InputPipeLength(Pipe& p) //считывание длины трубы
 {
-	system("cls"); //https://www.cyberforum.ru/cpp-beginners/thread30713.html?ysclid=l8bdnuacc7726489405
 	do
 	{
 		cout << "Введите длину трубы:"<<endl;
 		cin >> p.PipeLength;
-		if (cin.fail()) //проверка корректности данных
-		{
-			cin.clear();//сбрасываем биты ошибок
-			cin.ignore(1000, '\n');//сбрасываем буфер с некорректными данными
-			system("cls"); //чистим консоль
-			cout << "Введено некоректное число, пожалуйста, повторите попытку." << endl;
-		}
-		else if (p.PipeLength <= 0.)
-		{
-			system("cls");
-			cout << "Длина не может быть отрицательной или равной 0, пожалуйста, повторите попытку." << endl;
-		}
-	} while (cin.fail() || p.PipeLength <= 0.);
+	} while (CheckingPositiveDouble(p.PipeLength)==false);
 }
 
 void InputPipeDia(Pipe& p) //считывание диаметра трубы
 {
-	system("cls");
 	do
 	{
 		cout << "Введите диаметр трубы:" << endl;
 		cin >> p.PipeDia;
-		if (cin.fail())//проверка корректности данных
-		{
-			cin.clear();
-			cin.ignore(1000, '\n');
-			system("cls");
-			cout << "Введено некоректное число, пожалуйста, повторите попытку." << endl;
-		}
-		else if (p.PipeDia <= 0.)
-		{
-			system("cls");
-			cout << "Длина не может быть отрицательной или равной 0, пожалуйста, повторите попытку." << endl;
-		}
-	} while (cin.fail() || p.PipeDia <= 0.);
+		
+	} while (CheckingPositiveDouble(p.PipeDia)==false);
 }
 
 void InputPipeStatus(Pipe& p)//считывание статуса трубы
 {
-	system("cls");
-	double check;
 	do
 	{
-		cin.clear();
-		cin.ignore(1000, '\n');
 		cout << "Введите статус трубы(в ремонте - 0, в работоспособном состоянии - 1): " << endl;
-		cin >> check;
-		if (cin.fail() || (check != 0 && check != 1)) //проверка корректности данных
-		{
-			system("cls");
-			cout << "Указано некоректное число, пожалуйста, введите 0 или 1." << endl;
-		}
-		p.PipeStatus = check;
-	} while (cin.fail() || (check != 0 && check != 1));
-
+		cin >> p.PipeStatus;
+	} while (CheckingBool(p.PipeStatus)==false);
 }
 
-void InputPipe() //функциональная часть консоли
+
+void InputPipe(Pipe& p) //последовательность ввода данных по трубе
 {
-	Pipe p;
-	InputPipeLength(p);
-	InputPipeDia(p);
-	InputPipeStatus(p);
+	double check;
+	if (p.PipeLength != 0)
+	{
+		cout << "Вы уверены что хотите перезаписать данные по трубе?" << endl << "Введите 1 для подтверждения операции" << endl;
+		cin >> check;
+		if (check == true)
+		{
+			cout << "Создание трубы" << endl;
+			InputPipeLength(p);
+			InputPipeDia(p);
+			InputPipeStatus(p);
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore(1000, '\n');
+			return;
+		}
+	}
+	else
+	{
+		cout << "Создание трубы" << endl;
+		InputPipeLength(p);
+		InputPipeDia(p);
+		InputPipeStatus(p);
+	}
+
 } 
 
-int MainSharedConsole() //скилет функциональной части консоли
+
+int MainSharedConsole(Pipe& p) //скилет функциональной части консоли
 {
 	int item;
 	cin >> item;
 	switch (item)
 	{
 	case 1:
-		InputPipe();
+		InputPipe(p);
 		break;
 	case 2:
 		cout << "2. Добавить КС";
@@ -133,15 +154,18 @@ int MainSharedConsole() //скилет функциональной части консоли
 	}
 }
 
+
 void SharedConsole() //главная консоль
 {
+	Pipe p = {};
 	bool check = true;
 	do
 	{
 		TextSharedConsole();
-		check = MainSharedConsole();
+		check = MainSharedConsole(p);
 	} while ((check!=0));
 }
+
 
 int main() //точка входа в программу
 {
